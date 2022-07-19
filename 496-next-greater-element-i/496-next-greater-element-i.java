8b574cafbba0f8fac50e63311220db026d74c223
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-07-20 01:18:00 +0530

    Time: 5 ms (70.27%), Space: 44.2 MB (50.11%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -3,53 +3,44 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
 
-        
         HashMap<Integer, Integer> map = new HashMap<>();
 
-//      **   iterating the elements from the rare end will **   
-        
-        
-//         HashMap<Integer, Integer> map = new HashMap<>();
+        //      **   iterating the elements from the rare end will **
 
-//         for (int i = nums2.length - 1; i >= 0; i--) {
-//             while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-//                 stack.pop();
-//             }
+        //         HashMap<Integer, Integer> map = new HashMap<>();
 
-//             if (stack.isEmpty()) {
-//                 map.put(nums2[i], -1);
-//                 stack.push(nums2[i]);
-//             } else {
-//                 map.put(nums2[i], stack.peek());
-//                 stack.push(nums2[i]);
-//             }
-//         }
+        //         for (int i = nums2.length - 1; i >= 0; i--) {
+        //             while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+        //                 stack.pop();
+        //             }
 
-//         int[] arr = new int[nums1.length];
+        //             if (stack.isEmpty()) {
+        //                 map.put(nums2[i], -1);
+        //                 stack.push(nums2[i]);
+        //             } else {
+        //                 map.put(nums2[i], stack.peek());
+        //                 stack.push(nums2[i]);
+        //             }
+        //         }
 
-//         for (int i = 0; i < arr.length; i++) {
-//             arr[i] = map.get(nums1[i]);
-//         }
-        
-        
-        
-        
-        for (int i = 0 ; i < nums2.length ; i++) {
+        //         int[] arr = new int[nums1.length];
+
+        //         for (int i = 0; i < arr.length; i++) {
+        //             arr[i] = map.get(nums1[i]);
+        //         }
+
+        for (int i = 0; i < nums2.length; i++) {
             while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-               map.put(stack.pop(),nums2[i]);
-            }    
+                map.put(stack.pop(), nums2[i]);
+            }
             stack.push(nums2[i]);
-
         }
-        
+
         int[] arr = new int[nums1.length];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = map.getOrDefault(nums1[i],-1);
+            arr[i] = map.getOrDefault(nums1[i], -1);
         }
-        
-        
-        
-        
+
         return arr;
     }
 }
