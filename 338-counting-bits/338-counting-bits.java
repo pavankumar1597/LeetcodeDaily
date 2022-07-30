afce6dea542bc13a00ee76afecf0633400901c22
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-07-30 14:15:00 +0530

    Time: 2 ms (88.00%), Space: 48.4 MB (32.63%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] countBits(int n) {
         
-        
-        
-        
         int[] g = new int[n+1] ;
         
         
         for(int i = 1 ;i<= n; i++){
+            
+            //we have found a pattern , the count of setbit in every number is equal to the count of setbit in its half the number; 
+            //if the number is even then the count is equal to count of its half the number 
+            // if number is odd count of setbit in its half of the number and +1 ; 
+                
             g[i] = g[i>>1]+ (i&1);
                 
             
