b49330d4ab7a001fb13c2ada4cc25ef3fa2a69c2
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-08-04 22:12:03 +0530

    Time: 3 ms (63.74%), Space: 57.1 MB (6.60%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        
+        
+        //approach1 = hashtable pus  all the elements and take up ists frequency and iterate over its frequency
+        // sort it and find the middle element you will get the output // as since the number cunt is greater then the n/2 tiimes so the middle element is always            majority element 
+        
+        
+        //Boyer-Moore Voting Algorithm
+        //If we had some way of counting instances of the majority element as +1+1 and instances of any other element as -1−1, summing them would make it obvious that the majority element is indeed the majority element.
+        //the numbr which aits till the last is the majority element 
+        
+        
+        
+        
+        int c = 0 ; 
+        int major = 0 ;
+        for(int i = 0 ; i  <nums.length ; i++){
+            
+            if( c == 0 ){
+                major = nums[i] ;
+            }
+            
+            c += (major == nums[i])?1:-1;
+                
+            
+        }
+        
+        
+      return   major ;
+        
+    }
+}
