839bdb2acb49e664fa98622c735f60c5b73247b6
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-08-21 11:39:21 +0530

    Time: 9 ms (31.99%), Space: 41.8 MB (85.16%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -9,30 +9,16 @@ class Solution {
         int sc = 0 ; 
         int count = 0 ;
         
-//         for(Map.Entry<Character,Integer> entry : map.EntrySet()){
-            
-//         if(value%2 == 0 ){
-//             count++;
-//     }else if (sc == 0){
-//         count++;
-//         sc++;
-//     }        }
-        
-        
+
         
         
            for (int value : map.values()) {
     if(value%2 == 0 ){
         count += value;
-    }else if (sc == 0){
-        count += value;
+    }else {
+        count += (sc==0)?value:value-1;
         sc++;
-    }else if(sc == 1){
-         count += value-1;
-    
-    }
-       
-       
+    } 
 }
         
         return count ;
