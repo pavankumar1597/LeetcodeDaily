c84747cfbaa96439a3cbcbbb69f4deb3175a1760
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-08-25 09:25:25 +0530

    Time: 191 ms (50.17%), Space: 68.5 MB (48.08%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,59 +1,16 @@
 class Solution {
     public int countPrimes(int n) {
         
-//     if( n < 2) return 0 ; 
-//     if(n == 2) return 0 ;
-        
-//         int c = 1 ;
-        
-//         boolean[] isntprit = new boolean[n] ;
-        
-// for(int i = 3; i<n ; i=i+2){
-    
-//     if(!isntprit[i]){
-//         c++;
-//          for(int j = i*i; j < n ; j+=i){ 
-//              isntprit[j] = true ; 
-//          }
-//     }
-// }        
-        
-        
-        
-//         return c ;
-        
-        
-        
-           
-        //Sieve of Erosthonus
+          boolean check[]=new boolean[n];
         int count=0;
-        if(n<=2)
-            return 0; 
-        boolean[] primes=new boolean[n];
-        
-        Arrays.fill(primes,true);
-        primes[0]=false;
-        primes[1]=false;
-        
-        for(int i=2; i*i<=n;i++)
-        {
-            if(primes[i])
-            {
-                for(int j=i*i;j<n;j+=i)
-                {
-                    primes[j]=false;
+        for(int i=2;i<n;i++){
+            if(check[i]==false){
+                count++;
+                for(int j=i;j<n;j+=i){
+                    check[j]=true;
                 }
             }
         }
-        
-        for(int i=0;i<n;i++)
-        {
-            if(primes[i])
-            {
-                count++;
-            }
-        }
-        
-        return count;
+        return count;   
     }
 }
