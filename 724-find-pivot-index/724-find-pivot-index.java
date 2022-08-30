300b4554a5023219ccb876e2a92f465b11320589
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-08-31 00:21:07 +0530

    Time: 2 ms (78.73%), Space: 51.6 MB (82.34%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -5,14 +5,11 @@ class Solution {
             nums[i] = nums[i-1]+nums[i] ;
         }
         
+        
+        
         for( int i = 0 ; i < nums.length ; i++){
-            int right = 0;
-            if(i == 0 ) right = 0;
-            else right =nums[i-1];
-            
-            int left = nums[nums.length-1] - nums[i];
-            
-            if(left == right) return i ;
+          int  right = (i == 0 )? 0 :nums[i-1];
+            if(nums[nums.length-1] - nums[i] == right) return i ;
         }
         
         
