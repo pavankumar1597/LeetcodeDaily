f12611997ad6cd776f7f0f13842e3a986ed70588
Pavan kumar N <58086990+pavankumar1597@users.noreply.github.com>
2022-10-31 08:27:35 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (9.28%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -21,36 +21,13 @@ return  localpeak(nums,0,j);
                         return e ;
                     }
                 int m = s+(e-s)/2 ;
+
                 
-            if(nums[m] < nums[m+1]){
+  if(nums[m] < nums[m+1]){
                 return localpeak(nums,m+1,e);
             }else{
                 return localpeak(nums,s,m);
-            } 
-                
+            }
                 
     }
-    
-    
-    
-    
-    
-    
-          
-//         int s = 0 ;
-//         int e = A.length-1;
-
-//         while(s<e){
-//             int m = s+(e-s)/2;
-
-//             if(A[m] < A[m+1]){
-//                 s = m+1;
-//             }else{
-//                 e = m; 
-//             }
-//         }
-
-// return  A[s];
-
-
 }
